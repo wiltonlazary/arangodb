44b02085fecfa5d1b7e2350634e22b01dd56431e
Alan Plum <me@pluma.io>
2016-03-31 14:47:23 +0200

    Always run _scanFoxx in a transaction

--- js/server/modules/org/arangodb/foxx/manager.js
+++ js/server/modules/org/arangodb/foxx/manager.js
@@ -295,13 +295,20 @@ function checkMountedSystemApps(dbname) {
   var i, mount;
   var collection = utils.getStorage();
   for (i = 0; i < usedSystemMountPoints.length; ++i) {
-    mount = usedSystemMountPoints[i];
-    delete appCache[dbname][mount];
-    var definition = collection.firstExample({mount: mount});
-    if (definition !== null) {
-      collection.remove(definition._key);
-    }
-    _scanFoxx(mount, {});
+    db._executeTransaction({
+      collections: {
+        write: collection.name()
+      },
+      action() {
+        mount = usedSystemMountPoints[i];
+        delete appCache[dbname][mount];
+        var definition = collection.firstExample({mount: mount});
+        if (definition !== null) {
+          collection.remove(definition._key);
+        }
+        _scanFoxx(mount, {});
+      }
+    });
     executeAppScript('setup', lookupApp(mount));
   }
 }
@@ -901,7 +908,15 @@ function scanFoxx(mount, options) {
     [ [ 'Mount path', 'string' ] ],
     [ mount ] );
   initCache();
-  var app = _scanFoxx(mount, options);
+  var app;
+  db._executeTransaction({
+    collections: {
+      write: collection.name()
+    },
+    action() {
+      app = _scanFoxx(mount, options);
+    }
+  });
   reloadRouting();
   return app.simpleJSON();
 }
